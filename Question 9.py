ab277c511eb7d7da43243f5ef6ebb2921ac2aca6
chaitenyachand <129392157+chaitenyachand@users.noreply.github.com>
2024-07-04 11:15:27 +0530

    Create Question 9.py

---
+++ Question 9.py
@@ -0,0 +1,10 @@
+# Create a progran that stops printing numbers when it encounters a number greater than 5
+# using the break statement
+
+for i in range(1,11):
+  # Skip all numbers greater than 5
+  if i>5:
+    break
+  else:
+    print(i)
+    
