023f89c9dc2354456b657b579a82eef261f05392
chaitenyachand <129392157+chaitenyachand@users.noreply.github.com>
2024-07-04 10:38:59 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,6 @@
+# Create variables to store your name, age, and favourite hobby. Print these variables
+
+name = "Chaitenya Chand"
+age = 19
+fav_hobby = "Exercising"
+print(f"Hi, My name is {name}, my age is {age} and my favourite hobby is {fav_hobby}")
