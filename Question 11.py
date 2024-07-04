0ec67bb188ef6d3b79522e964789c34de0fa456d
chaitenyachand <129392157+chaitenyachand@users.noreply.github.com>
2024-07-04 11:20:58 +0530

    Create Question 11.py

---
+++ Question 11.py
@@ -0,0 +1,9 @@
+# Create a function that takes two numbers as arguments and returns their sum. 
+
+# function add_numbers
+def add_numbers(a, b):
+    return a + b
+
+# function call
+result = add_numbers(3, 5)
+print(result)
