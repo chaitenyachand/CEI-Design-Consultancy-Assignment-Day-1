c12e14ac86367cccd44d9db7e2b86f4a0e5099a5
chaitenyachand <129392157+chaitenyachand@users.noreply.github.com>
2024-07-04 10:46:04 +0530

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,11 @@
+# Add comments to your code explaining what each line does.
+
+# Program to print product of two number
+num1 = 23 # initialize num1
+num2 = 45 # initialize num2
+
+# Create a variable to pass the product of num1 and num2
+prod = num1*num2
+
+# Print the product of the two numbers
+print(f"Product of two numbers are {product} ")
